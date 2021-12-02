e36c732977b927b3d1f89db145e6397c298c5e6e
GeonwooVincentKim <kdsnop@gmail.com>
2021-12-02 11:25:00 +0900

    [Feature] - Successfully solved Button Click Issue

--- js/style.js
+++ js/style.js
@@ -6,3 +6,47 @@
         document.documentElement.scrollTop > 50 ?  isHeaderOn.classList.add('on') : isHeaderOn.classList.remove('on')
     });
 })();
+
+var isNavShow = false;
+function onClickToggleNavigation() {
+    this.isNavShow = !this.isNavShow;
+}
+
+function myFunction(){
+    var x = document.querySelector('.layout-header-gnb-area');
+    
+    if(x.style.display === "none"){
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+// (function() {
+//     window.addEventListener('click', function(){
+//         const isToggleClick = document.querySelector('.gnb-selector-toggle');
+        
+//         if(isNavShow == true){
+//             this.document.getElementsByClassName('layout-header-gnb-area').classList.add('state-click')
+//             this.document.getElementsByClassName('layout-header-gnb-area').style.display = "";
+//         } else {
+//             this.document.getElementsByClassName('layout-header-gnb-area').classList.remove('state-click')
+//             this.document.getElementsByClassName('layout-header-gnb-area').style.display = "none";
+//         }
+//     });
+// })();
+
+// function myFunction(){
+//     var x = document.querySelector('.gnb-selector-toggle');
+//     var y = document.querySelector('.layout-header-gnb-area');
+
+//     if(x.style.display == "none"){
+//         y.classList.add
+//         x.style.display = "block";
+//     }
+// }
+
+// $(function() {
+//     $(".gnb-selector-toggle").click(function(){
+//         $(".layout-header-gnb-area").toggle();
+//     });
+// });
