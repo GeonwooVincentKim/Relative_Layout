11b05c615be3191becbc87a8fb2ee08299904d01
GeonwooVincentKim <kdsnop@gmail.com>
2021-12-02 12:03:55 +0900

    [Feature] - layout-guide-container

--- js/style.js
+++ js/style.js
@@ -1,5 +1,3 @@
-// console.log("Testing..")
-
 (function () {
     window.addEventListener('scroll', function(){
         const isHeaderOn = document.querySelector('.layout-header');
@@ -7,46 +5,14 @@
     });
 })();
 
-var isNavShow = false;
-function onClickToggleNavigation() {
-    this.isNavShow = !this.isNavShow;
-}
-
-function myFunction(){
+function GNBClick(){
     var x = document.querySelector('.layout-header-gnb-area');
     
     if(x.style.display === "none"){
         x.style.display = "block";
+        x.classList.add('state-click');
     } else {
         x.style.display = "none";
+        x.classList.remove('state-click');
     }
 }
-// (function() {
-//     window.addEventListener('click', function(){
-//         const isToggleClick = document.querySelector('.gnb-selector-toggle');
-        
-//         if(isNavShow == true){
-//             this.document.getElementsByClassName('layout-header-gnb-area').classList.add('state-click')
-//             this.document.getElementsByClassName('layout-header-gnb-area').style.display = "";
-//         } else {
-//             this.document.getElementsByClassName('layout-header-gnb-area').classList.remove('state-click')
-//             this.document.getElementsByClassName('layout-header-gnb-area').style.display = "none";
-//         }
-//     });
-// })();
-
-// function myFunction(){
-//     var x = document.querySelector('.gnb-selector-toggle');
-//     var y = document.querySelector('.layout-header-gnb-area');
-
-//     if(x.style.display == "none"){
-//         y.classList.add
-//         x.style.display = "block";
-//     }
-// }
-
-// $(function() {
-//     $(".gnb-selector-toggle").click(function(){
-//         $(".layout-header-gnb-area").toggle();
-//     });
-// });
