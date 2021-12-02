1758b113179ce09c4d56d30eebad799fdab8e2cd
GeonwooVincentKim <kdsnop@gmail.com>
2021-12-02 09:13:39 +0900

    [Feature] - Layout Header

--- js/style.js
+++ js/style.js
@@ -1,1 +1,8 @@
-console.log("Testing..")
+// console.log("Testing..")
+
+(function () {
+    window.addEventListener('scroll', function(){
+        const isHeaderOn = document.querySelector('.layout-header');
+        document.documentElement.scrollTop > 50 ?  isHeaderOn.classList.add('on') : isHeaderOn.classList.remove('on')
+    });
+})();
