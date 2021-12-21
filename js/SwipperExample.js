15f5cb5b41b16a30933a999fe571b42a5f66241f
GeonwooVincentKim <kdsnop@gmail.com>
2021-12-21 21:29:47 +0900

    [Update] - Updated bullet-active tag

--- js/SwipperExample.js
+++ js/SwipperExample.js
@@ -1,7 +1,14 @@
 var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
+    },
+    autoPlay: {
+        delay: 3000,
+        disableOnInteraction: false
     },
     navigation: {
         nextEl: '.swiper-button-next',
