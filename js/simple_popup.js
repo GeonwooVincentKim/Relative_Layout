814284e91ae66ba5a28d6747554923b945233f26
GeonwooVincentKim <kdsnop@gmail.com>
2022-01-04 08:54:51 +0900

    [Feature & Modify] - Solved does not show correct popup js source

--- js/simple_popup.js
+++ js/simple_popup.js
@@ -1,8 +1,10 @@
 console.log("Access to Simple Popup js page")
 
-window.open(
-    'URL', "", 
-    'width=250px, height=300px, left=50%, top=50%'
-    .concat(' toolbar=0, status=yes, menubars=0,')
-    .concat('scrollbars=0, resizable=0, location=0, directories=0')
-)
+function Popup(){
+    window.open(
+        './js/simple_popup.js', "", 
+        'width=250px, height=300px, left=50%, top=50%'
+        .concat(' toolbar=0, status=yes, menubars=0,')
+        .concat('scrollbars=0, resizable=0, location=0, directories=0')
+    )    
+}
